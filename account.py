716a575c8b60a82b91fa207344c82924e37c2875
datchinguyen <118578428+datchinguyen@users.noreply.github.com>
2022-11-18 21:44:08 -0600

    Update account.py

--- account.py
+++ account.py
@@ -1,8 +1,23 @@
 class Account:
-    def __int__(self,name):
+    """
+    A class representing details for an account object
+    """
+    
+    def __init__(self, name: str) -> None:
+        """
+        Constructor to create initial state of an account object
+        :param name: Account's name
+        
+        """
         self.__account_name = name
         self.__account_balance = 0
-    def deposit(self, amount):
+        
+    def deposit(self, amount: int):
+        """
+        Method to check the amount 
+        :return True or False
+        """
+        
         if amount <= 0:
             de = False
             pass
@@ -14,7 +29,12 @@ class Account:
         else:
             return False
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: int):
+        """
+        Method to check amount
+        :return True or False
+        """
+        
         if amount <= 0 or amount > self.__account_balance:
             de = False
             pass
@@ -28,9 +48,19 @@ class Account:
         else:
             return False
 
-    def get_balance(self):
+    def get_balance(self) -> int:
+        """
+        Method to access a account's name
+        :return: Account's name
+        """
+        
         return self._account_balance
 
-    def get_name(self):
+    def get_name(self) -> str:
+        """
+        Method to access a account's name
+        :return: Account's name
+        """
+        
         return self._account_name
             
